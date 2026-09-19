1f4011f8b9fc743d2ed27cfb11ed810cb3987806
Anastasia <duval.ana51@gmail.com>
2026-09-19 23:10:01 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+def say_hi(name, age):
+    return f"Hi. My name is {name} and I'm {age} years old"
+
+
+assert say_hi("Alex", 32) == "Hi. My name is Alex and I'm 32 years old", 'Test1'
+assert say_hi("Ana", 28) == "Hi. My name is Ana and I'm 28 years old", 'Test2'
+
+print('ОК')
